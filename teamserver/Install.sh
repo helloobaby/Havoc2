f4adf5719b1adfc0795c0fb2df6d2960d472488f
helloobaby <sun888486@qq.com>
2023-11-14 17:43:47 +0800

    update

--- teamserver/Install.sh
+++ teamserver/Install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# rm /tmp/mingw-musl-32.tgz;rm /tmp/mingw-musl-64.tgz
+# 
 if [ ! -d "dir/x86_64-w64-mingw32-cross" ]; then
 	sudo apt -qq --yes install golang-go nasm mingw-w64 wget >/dev/null 2>&1
 
@@ -8,14 +9,16 @@ if [ ! -d "dir/x86_64-w64-mingw32-cross" ]; then
 	fi
 
 	if [ ! -f /tmp/mingw-musl-64.tgz ]; then
-		wget --no-check-certificate https://43.241.16.222:43337/Temp/x86_64-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-64.tgz
+		#wget --no-check-certificate https://43.241.16.222:43337/Temp/x86_64-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-64.tgz
+		wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -O /tmp/mingw-musl-64.tgz
 	fi
 
 
 	tar zxf /tmp/mingw-musl-64.tgz -C data
 
 	if [ ! -f /tmp/mingw-musl-32.tgz ]; then
-		wget --no-check-certificate https://43.241.16.222:43337/Temp/i686-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-32.tgz
+		#wget --no-check-certificate https://43.241.16.222:43337/Temp/i686-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-32.tgz
+		wget https://musl.cc/i686-w64-mingw32-cross.tgz -O /tmp/mingw-musl-32.tgz
 	fi
 
 	tar zxf /tmp/mingw-musl-32.tgz -C data
